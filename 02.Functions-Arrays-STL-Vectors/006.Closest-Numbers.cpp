581243f47d637399fc9d7b29460006eb36d70cc8
Irina Hristova <hristova119@gmail.com>
2019-09-15 21:22:03 +0300

    fix: check array length

--- 02.Functions-Arrays-STL-Vectors/006.Closest-Numbers.cpp
+++ 02.Functions-Arrays-STL-Vectors/006.Closest-Numbers.cpp
@@ -5,11 +5,16 @@ const int MaxLen = 100;
 
 int findClosestDifference(int arr[], int  len)
 {
+	if (len == 1)
+	{
+		return 0;
+	}
+
 	int min = INT_MAX;
 	int absVal = 0;
-	for (size_t i = 0; i < len; i++)
+	for (size_t i = 0; i < len - 1; i++)
 	{
-		for (size_t k = i + 1; k < len ; k++)
+		for (size_t k = i + 1; k < len; k++)
 		{
 			absVal = abs(arr[i] - arr[k]);
 			min = min > absVal ? absVal : min;
