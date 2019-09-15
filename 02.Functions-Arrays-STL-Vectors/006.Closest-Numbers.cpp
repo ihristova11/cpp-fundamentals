eb6ffa036d832fc730eeedfa35fbfddabd31dd18
Irina Hristova <hristova119@gmail.com>
2019-09-15 20:32:23 +0300

    feat: cpp sln to 006.

---
+++ 02.Functions-Arrays-STL-Vectors/006.Closest-Numbers.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cmath>
+
+const int MaxLen = 100;
+
+int findClosestDifference(int arr[], int  len)
+{
+	int min = INT_MAX;
+	int absVal = 0;
+	for (size_t i = 0; i < len; i++)
+	{
+		for (size_t k = i + 1; k < len ; k++)
+		{
+			absVal = abs(arr[i] - arr[k]);
+			min = min > absVal ? absVal : min;
+		}
+	}
+
+	return min;
+}
+
+int main()
+{
+	int n = 0;
+	int arr[MaxLen];
+
+	std::cin >> n;
+
+	for (size_t i = 0; i < n; i++)
+	{
+		std::cin >> arr[i];
+	}
+
+	std::cout << findClosestDifference(arr, n) << std::endl;
+
+	return 0;
+}
