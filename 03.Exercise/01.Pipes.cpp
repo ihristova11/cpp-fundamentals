7ca3cc85106fab3268fb5d1b3eb87692d4ed2837
Irina Hristova <hristova119@gmail.com>
2019-10-02 22:36:33 +0300

    feat: cpp sln to 01.Pipes

---
+++ 03.Exercise/01.Pipes.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+const int MaxLen = 500;
+
+int main()
+{
+	int checkup[MaxLen];
+	int installation[MaxLen];
+	int diff = 0;
+	int n;
+
+	std::cin >> n;
+
+	for (size_t i = 0; i < n; i++)
+	{
+		std::cin >> checkup[i];
+	}
+
+	for (size_t i = 0; i < n; i++)
+	{
+		std::cin >> installation[i];
+		diff = installation[i] - checkup[i];
+		std::cout << installation[i] / diff << " ";
+	}
+
+	return 0;
+}
