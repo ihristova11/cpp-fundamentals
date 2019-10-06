a7b94fd628e3367c28c26209f232abfff73aa92f
Irina Hristova <hristova119@gmail.com>
2019-10-06 12:37:00 +0300

    feat: add sln to 02.mathematical-expression

---
+++ 04.string-and-streams/02.mathematical-expression.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <stack>
+
+bool isCorrect(std::string input)
+{
+	std::stack<char> openingParanthesis;
+	int i = 0;
+
+	while (input[i] != '\0')
+	{
+		if (input[i] == '(')
+		{
+			openingParanthesis.push(i);
+		}
+		else if (input[i] == ')')
+		{
+			if (openingParanthesis.empty())
+			{
+				return false;
+			}
+			openingParanthesis.pop();
+		}
+		++i;
+	}
+
+	return openingParanthesis.empty();
+}
+
+int main()
+{
+	std::string input;
+	std::getline(std::cin, input);
+
+	std::string result = isCorrect(input) ? "correct" : "incorrect";
+
+	std::cout << result << std::endl;
+}
